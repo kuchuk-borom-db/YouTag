40da07425c11b4e09ab0501465044336b5dd5e86
kuchuk borom debbarma <kuchukboromdebbarma@outlook.com>
2024-11-08 12:19:34 +0530

    fixed circular dependency

--- backend/YouTagServer/src/main/java/dev/kuku/youtagserver/shared/application/Service.java
+++ backend/YouTagServer/src/main/java/dev/kuku/youtagserver/shared/application/Service.java
@@ -1,8 +1,8 @@
 package dev.kuku.youtagserver.shared.application;
 
-import dev.kuku.youtagserver.junction.api.exceptions.JunctionDTOHasNullValues;
-import dev.kuku.youtagserver.video.api.exceptions.VideoDTOHasNullValues;
+import dev.kuku.youtagserver.shared.exceptions.ResponseException;
 
+//TODO : Use common dto exception
 public interface Service<Entity, Dto> {
-    Dto toDto(Entity e) throws VideoDTOHasNullValues, JunctionDTOHasNullValues;
+    Dto toDto(Entity e) throws ResponseException;
 }
