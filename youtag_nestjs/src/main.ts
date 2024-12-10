87d34a4bc8e2466dc92d34138e0f6ee1d7d0b9f5
kuchuk borom debbarma <kuchukboromdebbarma@outlook.com>
2024-12-10 15:57:45 +0530

    Added nestjs and fastify adapter

---
+++ youtag_nestjs/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  //Open server to all devices connected to the same network
+  await app.listen(process.env.PORT ?? 3000,"0.0.0.0");
+}
+
+bootstrap();
