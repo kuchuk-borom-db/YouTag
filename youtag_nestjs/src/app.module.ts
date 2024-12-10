7efc16cd4c0706245e78abf9d4fe3215af351a33
kuchuk borom debbarma <kuchukboromdebbarma@outlook.com>
2024-12-10 17:13:53 +0530

    Using apollo and its playground

--- youtag_nestjs/src/app.module.ts
+++ youtag_nestjs/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphqlModule as GQL } from './graphql/graphql.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import * as path from 'node:path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: path.join(process.cwd(), 'src/Graphql/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    GQL,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
