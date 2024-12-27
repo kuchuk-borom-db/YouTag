41fa6ef220279311e1af2727459486456c3e166e
kuchuk borom debbarma <kuchukboromdebbarma@outlook.com>
2024-12-27 23:23:17 +0530

    updated

--- youtag_astro/astro.config.mjs
+++ youtag_astro/astro.config.mjs
@@ -1,5 +1,4 @@
-// @ts-check
-// @ts-check
+
 import {defineConfig} from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
@@ -9,21 +8,9 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-    //Prerender every page as static site by default
     output: "server",
 
     integrations: [tailwind(), react()],
 
-    experimental: {
-    },
-
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-        isr : true,
-
-        edgeMiddleware: true,
-        maxDuration: 8,
-    }),
+    adapter: vercel(),
 });
