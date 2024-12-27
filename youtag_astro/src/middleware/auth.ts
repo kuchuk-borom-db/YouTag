fa0fd195b26c343f40c0c17ca28e9da4d79759c2
kuchuk borom debbarma <kuchukboromdebbarma@outlook.com>
2024-12-27 16:45:32 +0530

    trying to fix

--- youtag_astro/src/middleware/auth.ts
+++ youtag_astro/src/middleware/auth.ts
@@ -1,24 +1,24 @@
-import {defineMiddleware} from "astro/middleware";
+import { defineMiddleware } from "astro/middleware";
 
-/**
- * Check if server-cookie contains token.
- */
 const logLabel = "Middleware-Auth"
 export const auth = defineMiddleware(async (context, next) => {
     const pathName = context.url.pathname;
     console.log(`Current page = ${pathName}`)
-    console.log(`Current full path  = ${context.url}`)
+    console.log(`Current full path = ${context.url}`)
 
     console.log(`Auth middleware triggered on path name ${pathName}`)
     if (!isAuthPath(pathName)) {
         console.log(`Hit a public route ${pathName}`);
         return next();
     }
+
     console.log(`Cookies : ${JSON.stringify(context.cookies)}`)
     const token = context.cookies.get("token")?.value
     if (token == undefined) {
-        console.log(`No token provided. Redirecting to login page ${context.url}`)
-        const response = context.redirect("login")
+        console.log(`No token provided. Redirecting to login page`)
+        // Add the current URL as a redirect parameter
+        const returnUrl = encodeURIComponent(context.url.pathname + context.url.search)
+        const response = context.redirect(`/login?redirect=${returnUrl}`)
         console.log(`Redirecting to ${response.url}`)
         return response;
     }
